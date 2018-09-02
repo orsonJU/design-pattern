008da52a6b09511ef1446f92c68eb2800b5e8793
Orson.h.s.zhu <orson.h.s.zhu@outlook.com>
2018-09-02 20:52:18 +0800

    product a define

---
+++ src/main/java/org/orson/factory/simple/ProductA.java
@@ -0,0 +1,15 @@
+package org.orson.factory.simple;
+
+/**
+ * Created by orson on 2018/9/2.
+ */
+public class ProductA implements  Product{
+
+    public String getName() {
+        return "ProductA";
+    }
+
+    public Integer getWight() {
+        return 50;
+    }
+}
